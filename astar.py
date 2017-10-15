bac804f7561b3e33fb984282c8d4376fe52c64bd
Sveinung Haugane <sveihaug@gmail.com>
2017-10-16 01:36:07 +0200

    Started making the algorithm

--- astar.py
+++ astar.py
@@ -1,9 +1,18 @@
 
 
-def astarAlgorithm(board):
-	g = 0 						#Cost to reach node
-	h = 0						#Cost from node to Goal
+def astarAlgorithm(board,startX,startY):
+	posX = startX
+	posY = startY
+	currentNode = board[startX][startY]
+	currentNode.cost = 0
 
-	print("yo")
-	print("Hello!")
-	return g
+	while not currentNode.end:
+		for edge in currentNode.edges:
+			for child in edge.connectedNodes:
+				if child != currentNode.self:
+					if (edge.cost + currentNode.cost) < child.cost:
+						child.cost = edge.cost + currentNode.cost
+						child.previousNode = self
+
+
+	return 0
