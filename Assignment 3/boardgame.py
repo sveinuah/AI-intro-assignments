0d8b4de3b8407a91bfbcf62c480fb653c8f57471
Sveinung Haugane <sveiaug@gmail.com>
2017-10-16 16:30:01 +0200

    Finished the structuring of the board

--- Assignment 3/boardgame.py
+++ Assignment 3/boardgame.py
@@ -1,69 +1,15 @@
 import astar
 
 fileList = ["boards/board-1-1.txt","boards/board-1-2.txt","boards/board-1-3.txt","boards/board-1-4.txt"]
-nodeArray = []
-numRows = 0
-numCols = 0
+formattedBoard = ""
 
+def findDistanceToGoal(nodeNum,goalNode,rowLength,colLength):
+	goalRow = int(goalNode/rowLength)
+	goalCol = int(goalNode%rowLength)
+	nodeRow = int(nodeNum/rowLength)
+	nodeCol = int(nodeNum%rowLength)
 
-class Node:
-	def __init__(self):
-		self.id = id(self)
-		self.cost = 10000
-		self.distanceToGoal = 10000
-		self.edges = []
-		self.previousNode = id(self)
-		self.obstacle = False
-		self.start = False
-		self.end = False
-
-class Edge:
-	def __init__(self,cost,node1,node2):
-		self.cost = cost
-		self.connectedNodes = [node1,node2]
-
-
-def findChildren():
-
-	for i in range(0,numRows):
-		for j in range(0,numCols):
-			children = []
-			ownId = nodeArray[i][j].id
-
-			if nodeArray[i][j].obstacle:
-				nodeArray[i][j].edges.append(children)
-			
-			else:
-				if i-1 >= 0 and not nodeArray[i-1][j].obstacle:
-					children.append(Edge(1,ownId,nodeArray[i-1][j].id))
-
-				if i+1 < numRows and not nodeArray[i+1][j].obstacle:
-					children.append(Edge(1,ownId,nodeArray[i+1][j].id))
-
-				if j-1 >= 0 and not nodeArray[i][j-1].obstacle:
-					children.append(Edge(1,ownId,nodeArray[i][j-1].id))
-
-				if j+1 < numCols and not nodeArray[i][j+1].obstacle:
-					children.append(Edge(1,ownId,nodeArray[i][j+1].id))
-
-				nodeArray[i][j].edges.append(children)
-
-
-
-def findDistanceToGoal():
-	goalPosX = -1
-	goalPosY = -1
-
-	for i in range(0,numRows):
-		for j in range(0,numCols):
-			if nodeArray[i][j].end == True:
-				goalPosX = i
-				goalPosY = j
-
-	for i in range(0,numRows):
-		for j in range(0,numCols):
-			nodeArray[i][j].distanceToGoal = abs(goalPosX-i) + abs(goalPosY-j)
-
+	return abs(goalRow-nodeRow) + abs(goalCol-nodeCol)
 
 def boardInit():
 	choice = int(input("Input number between 1-4 to choose a board: "))
@@ -74,48 +20,71 @@ def boardInit():
 	
 	print(contents)
 
-#-------------- Node making -------------------------
-	row = 0
-	col = 0
-	tempArray = []
+#-------------- formatting  -------------------------
+	rows = contents.split('\n')
+	rowNum = 0
+	colNum = 0
+	nodeNum = 0
+	goalNode = -1
+	nodeEdges = []
+	nodeGoalDistance = 0
+	nodeType = ""
 
-	for element in contents:
-		if element == '\n':
-			row = row + 1
-			col = 0
-			if len(tempArray) > 0:
-				nodeArray.append(tempArray)
-				tempArray = []
-		else:
-			tempArray.append(Node())
-			if(element == 'A'):
-				tempArray[col].start = True
-			elif element == 'B':
-				tempArray[col].end = True
-			elif element == '#':
-				tempArray[col].obstacle = True
-			col = col + 1
+	rowLength = len(rows[0]) #Assuming equal row length
+	colLength = len(rows) #Assuming equal column length
+	global formattedBoard
 
-	global numRows
-	global numCols
-	numRows = len(nodeArray)
-	numCols = len(nodeArray[0])					#Assuming all rows have equal number of elements
+	for row in rows:
+		for char in row:
+			if char ==  'B':
+				goalNode = colNum + rowLength*rowNum
 
-	findChildren()
-	findDistanceToGoal()
+			colNum += 1		
+		rowNum += 1
+		colNum = 0
+
+	rowNum = 0
+	colNum = 0
+
+	for row in rows:
+		for char in row:
+			nodeNum = colNum + (rowNum)*rowLength
+			nodeEdges = []
+			formattedBoard += (str(nodeNum)+',')
+	
+			if colNum > 0:
+				nodeEdges.append("1 "+str(nodeNum-1))
+			if colNum < rowLength-1:
+				nodeEdges.append("1 "+str(nodeNum+1))
+			if rowNum > 0:
+				nodeEdges.append("1 "+str(nodeNum - rowLength))
+			if rowNum < colLength-1:
+				nodeEdges.append("1 "+str(nodeNum + rowLength))
+
+			for edge in nodeEdges:
+				formattedBoard += (edge+':')
+			formattedBoard = formattedBoard[:(len(formattedBoard)-1)]
+			formattedBoard += ','
+
+			if char == '#':
+				formattedBoard += "10000,"
+			else:	
+				formattedBoard += str(findDistanceToGoal(nodeNum,goalNode,rowLength,colLength))+','
+
+			nodeType = char
+			formattedBoard += nodeType+'\n'
+
+			colNum += 1
+		rowNum += 1
+		colNum = 0
+	formattedBoard = formattedBoard[:(len(formattedBoard)-1)] #Remove last \n
 
 # ------------ Game -------------------------------
 
 def game():
+	print("Game!")
 
-	for i in range(0,numRows):
-		for j in range(0,numCols):
-			if nodeArray[i][j].start:
-				startX = i
-				startY = j
-
-
-	shortestPath = astar.astarAlgorithm(nodeArray,startX,startY)
+	shortestPath = astar.astarAlgorithm(formattedBoard)
 
 boardInit()
 game()
