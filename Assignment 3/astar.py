ffc5bfd32bdd8984809ccb4c1acdfe0c1cd3aa93
Sveinung Haugane <sveiaug@gmail.com>
2017-10-16 13:48:03 +0200

    finished implementing node-structure and init function

--- Assignment 3/astar.py
+++ Assignment 3/astar.py
@@ -1,18 +1,55 @@
+class Node:
+	def __init__(self,id,dist):
+		self.id = id
+		self.cost = 10000
+		self.distanceToGoal = dist
+		self.edges = []
+		self.previousNode = id
+		self.start = False
+		self.end = False
 
+class Edge:
+	def __init__(self,cost,node):
+		self.cost = cost
+		self.connectedNode = node
 
-def astarAlgorithm(board,startX,startY):
-	posX = startX
-	posY = startY
-	currentNode = board[startX][startY]
-	currentNode.cost = 0
+def astarInit(board):
+	stringParts = {'id':0,'edges':1,'distanceToGoal':2,'nodeType':3}
 
-	while not currentNode.end:
-		for edge in currentNode.edges:
-			for child in edge.connectedNodes:
-				if child != currentNode.self:
-					if (edge.cost + currentNode.cost) < child.cost:
-						child.cost = edge.cost + currentNode.cost
-						child.previousNode = self
+	nodes  = board.split('\n')
+	nodeArray = []
+
+	for string in nodes:
+		attributes = string.split(',')
+		node = Node(attributes[stringParts['id']],attributes[stringParts['distanceToGoal']])
+		nodeType = attributes[stringParts['nodeType']]
+
+		if nodeType == 'A':
+			node.start = True
+		elif nodeType == 'B':
+			node.end = True
+
+		edges = attributes[stringParts['edges']].split(':')
+		for edge in edges:
+			edge = edge.split(" ")
+			node.edges.append(Edge(edge[0],edge[1]))
+
+		nodeArray.append(node)
+	
+	return nodeArray
+
+########################################################################################
+################                  PUBLIC FINCTIONS             #########################
+########################################################################################
+
+#Board-format should be a comma-separated string with:
+#<nodeNumber>,<edgecost1><space><connected node>:<edgecost2><space><connecten node 2>:...,<distance to goal>,<A = start or B = goal\n
+#<nodeNumber>,...
+
+def astarAlgorithm(board):
+	nodeArray = astarInit(board)
+	print(nodeArray)
+
 
 
 	return 0
