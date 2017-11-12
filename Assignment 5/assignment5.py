e97851bbe4d9be05d49e2e47b5c919b0dc8b5a8b
Sveinung Haugane <sveihaug@gmail.com>
2017-11-12 04:08:52 +0100

    Added count of failed calls and number of backtracks + fixed removal bug

--- Assignment 5/assignment5.py
+++ Assignment 5/assignment5.py
@@ -3,6 +3,9 @@
 import copy
 import itertools
 
+failures = 0
+backtrackCalls = 0
+
 class CSP:
     def __init__(self):
         # self.variables is a list of the variable names in the CSP
@@ -69,46 +72,17 @@ class CSP:
                 self.add_constraint_one_way(i, j, lambda x, y: x != y)
 
     def backtracking_search(self):
-        """This functions starts the CSP solver and returns the found
-        solution.
-        """
-        # Make a so-called "deep copy" of the dictionary containing the
-        # domains of the CSP variables. The deep copy is required to
-        # ensure that any changes made to 'assignment' does not have any
-        # side effects elsewhere.
+
         assignment = copy.deepcopy(self.domains)
-
-        # Run AC-3 on all constraints in the CSP, to weed out all of the
-        # values that are not arc-consistent to begin with
         self.inference(assignment, self.get_all_arcs())
-
-        # Call backtrack with the partial assignment 'assignment'
         return self.backtrack(assignment)
 
     def backtrack(self, assignment):
-        """The function 'Backtrack' from the pseudocode in the
-        textbook.
+        
+        global backtrackCalls
+        global failures
 
-        The function is called recursively, with a partial assignment of
-        values 'assignment'. 'assignment' is a dictionary that contains
-        a list of all legal values for the variables that have *not* yet
-        been decided, and a list of only a single value for the
-        variables that *have* been decided.
-
-        When all of the variables in 'assignment' have lists of length
-        one, i.e. when all variables have been assigned a value, the
-        function should return 'assignment'. Otherwise, the search
-        should continue. When the function 'inference' is called to run
-        the AC-3 algorithm, the lists of legal values in 'assignment'
-        should get reduced as AC-3 discovers illegal values.
-
-        IMPORTANT: For every iteration of the for-loop in the
-        pseudocode, you need to make a deep copy of 'assignment' into a
-        new variable before changing it. Every iteration of the for-loop
-        should have a clean slate and not see any traces of the old
-        assignments and inferences that took place in previous
-        iterations of the loop.
-        """
+        backtrackCalls += 1
 
         node = self.select_unassigned_variable(assignment)
         if  node == -1: return assignment
@@ -123,17 +97,11 @@ class CSP:
                 if result != False:
                     return result
 
-            assignment[node].remove(string(number))
-
+        failures += 1
         return False
 
     def select_unassigned_variable(self, assignment):
-        """The function 'Select-Unassigned-Variable' from the pseudocode
-        in the textbook. Should return the name of one of the variables
-        in 'assignment' that have not yet been decided, i.e. whose list
-        of legal values has a length greater than one.
-        """
-        # TODO: IMPLEMENT THIS
+
         for node in assignment.keys():
             if len(assignment[node]) > 1:
                 return node
@@ -141,12 +109,6 @@ class CSP:
         return -1
 
     def inference(self, assignment, queue):
-        """The function 'AC-3' from the pseudocode in the textbook.
-        'assignment' is the current partial assignment, that contains
-        the lists of legal values for each undecided variable. 'queue'
-        is the initial queue of arcs that should be visited.
-        """
-        # TODO: IMPLEMENT THIS
 
         while len(queue):
             Xi, Xj = queue.pop(0)
@@ -159,14 +121,7 @@ class CSP:
         return True
 
     def revise(self, assignment, i, j):
-        """The function 'Revise' from the pseudocode in the textbook.
-        'assignment' is the current partial assignment, that contains
-        the lists of legal values for each undecided variable. 'i' and
-        'j' specifies the arc that should be visited. If a value is
-        found in variable i's domain that doesn't satisfy the constraint
-        between i and j, the value should be deleted from i's list of
-        legal values in 'assignment'.
-        """
+
         revised = False
 
         for x in assignment[i]:
@@ -242,8 +197,36 @@ def print_sudoku_solution(solution):
             print '------+-------+------'
 
 
+print "EASY:"
 sudoku = create_sudoku_csp("sudokus/easy.txt")
-print_sudoku_solution(sudoku.domains)
 solution = sudoku.backtracking_search()
-print "------------------------------"
-print_sudoku_solution(solution) 
+if solution: print_sudoku_solution(solution) 
+print "Failures = %d" % failures
+print "Backtrack calls = %d" % backtrackCalls
+failures = 0
+backtrackCalls = 0
+
+print "MEDIUM:"
+sudoku = create_sudoku_csp("sudokus/medium.txt")
+solution = sudoku.backtracking_search()
+if solution: print_sudoku_solution(solution) 
+print "Failures = %d" % failures
+print "Backtrack calls = %d" % backtrackCalls
+failures = 0
+backtrackCalls = 0
+
+print "HARD:"
+sudoku = create_sudoku_csp("sudokus/hard.txt")
+solution = sudoku.backtracking_search()
+if solution: print_sudoku_solution(solution) 
+print "Failures = %d" % failures
+print "Backtrack calls = %d" % backtrackCalls
+failures = 0
+backtrackCalls = 0
+
+print "VERY HARD:"
+sudoku = create_sudoku_csp("sudokus/veryhard.txt")
+solution = sudoku.backtracking_search()
+if solution: print_sudoku_solution(solution) 
+print "Failures = %d" % failures
+print "Backtrack calls = %d" % backtrackCalls
